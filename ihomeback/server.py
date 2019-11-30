55141c2125156e5ef5420386c054f39dcd4ea756
vishnu <vishnu44d@gmail.com>
2019-12-01 02:18:07 +0530

    adding initial files

---
+++ ihomeback/server.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask
+from flask_mqtt import Mqtt
+
+app = Flask(__name__)
+
+app.config['MQTT_BROKER_URL'] = 'mqtt'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_REFRESH_TIME'] = 1.0
+
+mqtt = Mqtt(app)
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    mqtt.subscribe('sensors/thermometer/1')
+
+
+
+@app.route('/', methods=['GET'])
+def root():
+    try:
+        pub_data = {
+            "data1": "value1"
+        }
+        mqtt.publish("sensors/thermometer/1", pub_data)
+        return "<h1>Hello</h1>"
+    except:
+        return "<h1>World</h1>"
+
+if __name__ == "__main__":
+    app.run(port=os.environ.get("FLASK_PORT", "9991"))
