eb3fd4ced64c648468315bc3402e517bca162613
vishnu <vishnu44d@gmail.com>
2019-12-02 00:19:28 +0530

    fix 9

--- ihomeback/server.py
+++ ihomeback/server.py
@@ -70,4 +70,4 @@ if __name__ == "__main__":
     print("Starting the server")
         
     client.loop_forever()
-    app.run(host='0.0.0.0', port=os.environ.get("FLASK_PORT", "80"), debug=False, use_reloader=False)
+    app.run()
