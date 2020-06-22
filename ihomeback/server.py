6ec4c41e7e47fa7f5653bd72a50d6b76d2b1664b
vishnu <vishnu44d@gmail.com>
2020-06-22 08:27:39 +0530

    updated random test for simulation

--- ihomeback/server.py
+++ ihomeback/server.py
@@ -1,3 +1,10 @@
+import paho.mqtt.client as mqtt
+from flask_cors import CORS
+from ihomeback.models import createTables, destroyTables
+from ihomeback.config import DbEngine_config
+from ihomeback import create_db_engine, create_db_sessionFactory
+from ihomeback.api import deviceBP
+from ihomeback.api import userBP
 import os
 from flask import Flask
 import json
@@ -5,18 +12,11 @@ import random
 import sys
 sys.path.insert(0, os.getcwd())
 # from flask_mqtt import Mqtt
-from ihomeback.api import userBP
-from ihomeback.api import deviceBP
-from ihomeback import create_db_engine, create_db_sessionFactory
-from ihomeback.config import DbEngine_config
-from ihomeback.models import createTables, destroyTables
-from flask_cors import CORS
-
 
 
 app = Flask(__name__)
 engine = create_db_engine(DbEngine_config)
-#destroyTables(engine)
+# destroyTables(engine)
 createTables(engine)
 
 SessionFactory = create_db_sessionFactory(engine)
@@ -34,38 +34,39 @@ app.config['MQTT_KEEP_ALIVE'] = 5
 #     print("Connecting...")
 #     mqtt.subscribe('sensors/thermometer/1')
 
-import paho.mqtt.client as mqtt
-import sys
 
-def on_connect(client, userdata, flags, rc):    
+def on_connect(client, userdata, flags, rc):
     print("Result from connect: {}".format(
-            mqtt.connack_string(rc)))    
-    # Subscribe to the senors/alitmeter/1 topic filter 
+        mqtt.connack_string(rc)))
+    # Subscribe to the senors/alitmeter/1 topic filter
     from topics import topics
     for topic in topics:
-        client.subscribe(topic)  
+        client.subscribe(topic)
 
-def on_subscribe(client, userdata, mid, granted_qos):    
+
+def on_subscribe(client, userdata, mid, granted_qos):
     print("I've subscribed")
 
 
-def on_message(client, userdata, msg):   
-    ## insert data into database
+def on_message(client, userdata, msg):
+    # insert data into database
 
     print("Message received. Topic: {}. Payload: {}".format(
-            msg.topic, str(msg.payload)))
+        msg.topic, str(msg.payload)))
+
 
 def on_disconnect(client, userdata, rc):
     print("Reconnecting")
     client.connect(host="mqtt", port=1883)
 
-client = mqtt.Client(protocol=mqtt.MQTTv311)    
-client.on_connect = on_connect    
-client.on_subscribe = on_subscribe    
+
+client = mqtt.Client(protocol=mqtt.MQTTv311)
+client.on_connect = on_connect
+client.on_subscribe = on_subscribe
 client.on_message = on_message
-client.on_disconnect = on_disconnect    
+client.on_disconnect = on_disconnect
 client.connect(host="mqtt", port=1883)
- 
+
 
 CORS(app, supports_credentials=True)
 @app.route('/', methods=['GET'])
@@ -75,31 +76,32 @@ def root():
             {
                 "measurement": "temperature",
                 "tags": {
-                    "host": "server04"
+                    "host": "server0{}".format(random.randint(1, 4))
                 },
                 "fields": {
-                    "value": random.randint(30,60),
+                    "value": random.randint(30, 60),
                 }
             }
         ]
-        client.publish("sensors/thermometer/1", json.dumps(json_body)).wait_for_publish()
+        client.publish("sensors/thermometer/1",
+                       json.dumps(json_body)).wait_for_publish()
         json_body = [
             {
                 "measurement": "humidity",
                 "tags": {
-                    "host": "server04"
+                    "host": "server{}".format(random.randint(1, 4))
                 },
                 "fields": {
-                    "value": random.randint(80,100),
+                    "value": random.randint(80, 100),
                 }
             }
         ]
-        client.publish("sensors/humidity/1", json.dumps(json_body)).wait_for_publish()
+        client.publish("sensors/humidity/1",
+                       json.dumps(json_body)).wait_for_publish()
         return "<h1>Hello</h1>"
     except Exception as e:
         print(str(e))
         return "<h1>World</h1>"
-    
 
 
 app.register_blueprint(userBP, url_prefix='/user')
@@ -107,6 +109,5 @@ app.register_blueprint(deviceBP, url_prefix='/device')
 
 if __name__ == "__main__":
     print("Starting the backend server")
-    app.run(host="0.0.0.0", use_reloader=False) 
+    app.run(host="0.0.0.0", use_reloader=False)
     client.loop_forever()
-    
